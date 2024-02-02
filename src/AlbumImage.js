8b761d96e58f80cec7d24d5902d0f23737dc4ae6
zyke <zyke.victoria.cics@ust.edu.ph>
2024-02-02 19:56:56 +0800

    Updated album look (more accurate)

--- src/AlbumImage.js
+++ src/AlbumImage.js
@@ -2,9 +2,15 @@ import React from "react";
 
 const AlbumImage = ({ imageUrl, title }) => {
   return (
-    <div className="album-image">
-      <img src={imageUrl} alt={title} />
-      <p className="image-title">{title}</p>
+    <div className="Gallery-Content--mediaContainer">
+      <div className="Gallery-Content--media">
+        <div className="imageContainer">
+          <img src={imageUrl} alt={title} />
+        </div>
+      </div>
+      <div className="Gallery-Content--descr">
+        <p className="image-title">{title}</p>
+      </div>
     </div>
   );
 };
